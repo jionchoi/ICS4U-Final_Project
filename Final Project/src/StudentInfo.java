f1408c27c3e9d5952dbf3fb1e4767f9f4a49d468
Jion Choi <118484265+jionchoi@users.noreply.github.com>
2023-05-18 18:33:55 -0400

    Update StudentInfo.java

--- Final Project/src/StudentInfo.java
+++ Final Project/src/StudentInfo.java
@@ -1,5 +1,54 @@
-public class StudentInfo {
-    public StudentInfo(){
+import javax.swing.*;
 
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class StudentInfo extends JFrame implements ActionListener {
+    public addPlayer addPlayer;
+    public searchPlayer searchPlayer;
+    public StudentInfo(){
+        JPanel pane = new JPanel();
+        pane.setSize(400,400);
+        pane.setLayout(new BorderLayout());
+        pane.setOpaque(false);
+        setContentPane(pane);
+
+        //Add Labelled input fields to display
+        JPanel inFieldPane = new JPanel();
+        inFieldPane.setLayout(new GridLayout(3,1));
+        inFieldPane.add(new JLabel("Welcome to Ball Bricks Game!"));
+        pane.add(inFieldPane,BorderLayout.NORTH);
+
+        //Add submission button
+        JPanel submitPane = new JPanel();
+        submitPane.setLayout(new FlowLayout());
+        JButton submitButton = new JButton("Add Student");
+        JButton tuto = new JButton("Search Student");
+        JButton back = new JButton("Back");
+
+        //This statement tells java to listen to the button
+        submitButton.addActionListener(this);
+        tuto.addActionListener(this);
+        submitPane.add(submitButton);
+        submitPane.add(tuto);
+        submitPane.add(back);
+        pane.add(submitPane,BorderLayout.CENTER);
+
+        //Display the final product
+        pack();
+        setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getActionCommand().equals("Add student")){
+            addPlayer = new addPlayer();
+            this.setVisible(false);
+        }
+        else if(e.getActionCommand().equals("Search Student")){
+            searchPlayer = new searchPlayer();
+            this.setVisible(false);
+        }
     }
 }
